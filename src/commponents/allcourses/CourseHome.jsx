7447b0c5bf43c7856ac60209947d6c8eceb9488c
Aisha <111194434+Aisha-Saad@users.noreply.github.com>
2024-12-08 20:18:24 +0300

    update

---
+++ src/commponents/allcourses/CourseHome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Back from '../commen/back/Back'
+import CourseCard from './CourseCard'
+
+const CourseHome = () => {
+  return (
+    <>
+    <Back title="explore couses"/>
+    <CourseCard />
+    </>
+  )
+}
+
+export default CourseHome
